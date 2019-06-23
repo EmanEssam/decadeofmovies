b39b7996a750c73aad4725104481039c21d5cc93
eman <emanessam05@gmail.com>
2019-06-23 06:29:58 +0200

    add MovieViewModel unit testing

---
+++ app/src/test/java/com/swvl/decadeofmovies/ui/viewmodel/MovieViewModelTest.kt
@@ -0,0 +1,64 @@
+package com.swvl.decadeofmovies.ui.viewmodel
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.swvl.decadeofmovies.data.model.Movie
+import com.swvl.decadeofmovies.data.repository.MovieRepository
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MovieViewModelTest {
+
+
+    lateinit var movieViewModel: MovieViewModel
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var movieRepository: MovieRepository
+
+
+    var movie: MutableLiveData<List<Movie>> = MutableLiveData()
+
+    @Mock
+    lateinit var context: Context
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        context = mock(Context::class.java)
+
+        movieViewModel = MovieViewModel(movieRepository)
+
+        val movies = listOf(Movie(0, "fight club", "", emptyList(), emptyList(), 1))
+
+        movie.value = movies
+
+
+    }
+
+    @Test
+    fun validateLocalMoviesData_isNotEmpty_ReturnTrue() {
+
+
+        `when`(movieRepository.getLocalMovies()).thenReturn(movie)
+
+        val result = movieViewModel.getLocalMovies()
+
+        assertTrue(result.value!!.isNotEmpty())
+
+    }
+}
